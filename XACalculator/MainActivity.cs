47da9f4853dd59b5403ed5c41eaa4f2068b42355
Hector Tran <16048463+HectorTran97@users.noreply.github.com>
2019-07-31 22:52:38 +1000

    + Add some codes for operation function + Re-design the UX/UI for the app

--- XACalculator/MainActivity.cs
+++ XACalculator/MainActivity.cs
@@ -4,6 +4,7 @@ using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
 using Android.Views;
+using System.Collections.Generic;
 
 namespace XACalculator
 {
@@ -15,7 +16,10 @@ namespace XACalculator
 
         private readonly string[] numbers = new string[2];
         private string @operator;
-
+        
+        //private IList<string> operatorList = new List<string>() { "÷", "×", "+", "-" };
+        //private string inputText;
+        
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -31,5 +35,135 @@ namespace XACalculator
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
+
+        [Java.Interop.Export("ButtonClick")]
+        public void ButtonClick(View v)
+        {
+            Button button = (Button)v;            
+
+            if ("0123456789.".Contains(button.Text))
+            {
+                AddDigitOrDecimalPoint(button.Text);
+            }
+            else if ("÷×+-".Contains(button.Text))
+            {
+                AddOperator(button.Text);
+            }
+            else if ("=" == button.Text)
+            {
+                Calculate();
+            }
+            else
+            {
+                Clear();
+            }
+        }
+
+        //private bool IsValid(string equationText, char input, int index)
+        //{
+        //    foreach (var item in operatorList)
+        //    {
+        //        if (inputText != item)
+        //        {
+        //            return false;
+        //        }
+        //    }
+        //}
+
+        private void AddDigitOrDecimalPoint(string value)
+        {
+            int index = (@operator == null) ? 0 : 1;
+
+            if (value == "." && numbers[index].Contains("."))
+            {
+                return;
+            }
+
+            numbers[index] += value;
+
+            UpdateCalculatorText();
+        }
+
+        private void AddOperator(string value)
+        {
+            if (numbers[1] != null)
+            {
+                Calculate(value);
+                return;
+            }
+
+            @operator = value;
+
+            UpdateCalculatorText();
+        }
+
+        private void Calculate(string newOperator = null)
+        {
+            double? result = null;
+            double? first = numbers[0] == null ? null : (double?)double.Parse(numbers[0]);
+            double? second = numbers[1] == null ? null : (double?)double.Parse(numbers[1]);
+
+            switch (@operator)
+            {
+                case "÷":
+                    result = first / second;
+                    break;
+                case "×":
+                    result = first * second;
+                    break;
+                case "+":
+                    result = first + second;
+                    break;
+                case "-":
+                    result = first - second;
+                    break;
+            }
+
+            if (result != null)
+            {
+                numbers[0] = result.ToString();
+                @operator = newOperator;
+                numbers[1] = null;
+                UpdateCalculatorText();
+            }
+        }
+
+        private void Clear()
+        {
+            numbers[0] = numbers[1] = null;
+            @operator = null;
+            UpdateCalculatorText();
+        }
+
+        //private void BackSpace()
+        //{
+        //    newText = string.Join("", numbers);
+
+        //    for (int i = newText.Length - 1; i >= 0; i--)
+        //    {
+        //        if (newText == null || newText == " ")
+        //        {
+        //            Clear();
+        //        }
+        //        else
+        //        {
+        //            if (newText.Length != 0)
+        //            {
+        //                newText = newText.Remove(newText.Length - 1, 1);
+        //                if (newText == null)
+        //                {
+        //                    calculatorText.Text = "0";
+        //                }
+
+        //                numbers[0] = newText;
+        //                @operator = null;
+        //                numbers[1] = null;
+        //                UpdateCalculatorText();
+        //            }
+        //        }
+        //    }
+        //}
+
+        private void UpdateCalculatorText() => calculatorText.Text = $"{numbers[0]} {@operator} {numbers[1]}";
     }
 }
